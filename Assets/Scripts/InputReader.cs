078d5e7b2ed626ba14ec3002f5907d70bb6dc65a
Anatoliy <sergeevbiz@ya.ru>
2026-01-15 10:25:29 +0300

    15/01 Добавлены константы клавиш мыши

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class InputReader : MonoBehaviour
 {
+    private const int LeftMouseButton = 0;
+    private const int RightMouseButton = 1;
+    private const int MiddleMouseButton = 2;
+
     public event Action WorkStateSwitched;
 
     private void Update()
@@ -12,7 +16,7 @@ public class InputReader : MonoBehaviour
 
     private void GetMouseClick()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
+        if (Input.GetMouseButtonDown(LeftMouseButton) || Input.GetMouseButtonDown(RightMouseButton) || Input.GetMouseButtonDown(MiddleMouseButton))
             WorkStateSwitched?.Invoke();
     }
 }
